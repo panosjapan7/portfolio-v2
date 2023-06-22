5b18c825f110ed6c4cc67e96389d7db8038633b8
panosjapan7 <panosjapan@gmail.com>
2023-06-22 10:57:25 +0200

    Corrects size of menu

--- app/components/MenuDesktop.tsx
+++ app/components/MenuDesktop.tsx
@@ -6,12 +6,13 @@ import {
   useTransform,
 } from "framer-motion";
 import Link from "next/link";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { PiLinkSimpleBold } from "react-icons/pi";
 import { FaHouse, FaUser } from "react-icons/fa6";
 
 const MenuDesktop = () => {
   let mouseX = useMotionValue(Infinity);
+  const [activePage, setActivePage] = useState(0);
 
   return (
     <div className="absolute bottom-20 end-0.5 right-0 left-0 mx-auto w-max ">
@@ -21,14 +22,30 @@ const MenuDesktop = () => {
         className="mx-auto flex h-16 items-end gap-4 rounded-2xl px-4 pb-3"
       >
         {[...Array(3).keys()].map((i) => (
-          <AppIcon mouseX={mouseX} key={i} i={i} />
+          <AppIcon
+            mouseX={mouseX}
+            key={i}
+            i={i}
+            active={activePage === i}
+            onClick={() => setActivePage(i)}
+          />
         ))}
       </motion.div>
     </div>
   );
 };
 
-function AppIcon({ mouseX, i }: { mouseX: MotionValue; i: number }) {
+function AppIcon({
+  mouseX,
+  i,
+  active,
+  onClick,
+}: {
+  mouseX: MotionValue;
+  i: number;
+  active: boolean;
+  onClick: () => void;
+}) {
   let ref = useRef<HTMLDivElement>(null);
 
   let distance = useTransform(mouseX, (val) => {
@@ -37,23 +54,48 @@ function AppIcon({ mouseX, i }: { mouseX: MotionValue; i: number }) {
     return val - bounds.x - bounds.width / 2;
   });
 
-  let widthSync = useTransform(distance, [-150, 0, 150], [40, 100, 40]);
+  let widthSync = useTransform(distance, [-100, 0, 100], [60, 100, 60]);
   let width = useSpring(widthSync, { mass: 0.1, stiffness: 150, damping: 12 });
 
   const links: string[] = ["/", "/about", "/links"];
   const icons = [FaHouse, FaUser, PiLinkSimpleBold];
   const IconComponent = icons[i];
 
+  const handleClick = () => {
+    onClick();
+  };
+
   return (
     <motion.div
       ref={ref}
-      style={{ width }}
-      className="cursor-pointer flex justify-center items-center"
+      style={{
+        width,
+      }}
+      className="cursor-pointer flex justify-center items-center rounded-full"
+      onClick={handleClick}
     >
-      <Link href={links[i]} style={{ height: "100%", width: "100%" }}>
-        <IconComponent
-          style={{ width: "80%", height: "80%", color: "#9B9B9B" }}
-        />
+      <Link
+        href={links[i]}
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            borderRadius: "50%",
+            border: active ? "2px solid #d1d1d1" : "none",
+            padding: 14,
+          }}
+        >
+          <IconComponent
+            style={{
+              width: "100%",
+              height: "100%",
+              color: "#9B9B9B",
+            }}
+          />
+        </div>
       </Link>
     </motion.div>
   );
