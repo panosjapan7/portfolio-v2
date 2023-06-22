6078b838f8c6f4e498306f39cfe7f88c894cb165
panosjapan7 <panosjapan@gmail.com>
2023-06-22 11:31:09 +0200

    Creates Header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex items-center p-5">
+        <img
+          className=" h-10 rounded-full mr-4"
+          alt="Panos"
+          src="/images/panos-1-square.jpg"
+        />
+        <div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis nam
+            et atque quae modi quia culpa amet molestias. Modi ipsum enim ea id
+            eos repellendus perspiciatis voluptatem? Praesentium, provident
+            numquam.
+          </p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
