ea91195f5f94a4a71a3dab83d68e4297509d036c
panosjapan7 <panosjapan@gmail.com>
2023-06-29 17:18:56 +0200

    Uses re-usable MenuItem

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import "./about.css";
 import { useState } from "react";
+import MenuItem from "./components/MenuItem";
 
 const AboutPage = () => {
   const [selectedTab, setSelectedTab] = useState("about-me");
@@ -11,52 +12,32 @@ const AboutPage = () => {
       <div className="left-column">
         <div>
           <h2 className="text-xl font-semibold mb-5">About Me</h2>
+
           <div>
+            <MenuItem
+              itemValue="about-me"
+              selectedTab={selectedTab}
+              setSelectedTab={setSelectedTab}
+              image="panos-2-square.jpg"
+              imageAlt="Panos"
+              menuText="About Me"
+              imageWidth={22}
+              imageHeight={22}
+            />
+            <MenuItem
+              itemValue="tech-skills"
+              selectedTab={selectedTab}
+              setSelectedTab={setSelectedTab}
+              image="tech-star.svg"
+              imageAlt="Tech icon"
+              menuText="Technical Skills"
+              imageWidth={22}
+              imageHeight={22}
+            />
             <div
-              className={`flex items-center gap-2 mb-5 relative cursor-pointer ${
-                selectedTab === "about-me" ? "cursor-default" : "cursor-pointer"
-              }`}
-              onClick={() => setSelectedTab("about-me")}
-            >
-              <Image
-                className="rounded border border-white z-10"
-                src="/images/panos-2-square.jpg"
-                width={23}
-                height={23}
-                alt="Panos"
-              />
-              <p className="text-xs font-medium z-10">About Me</p>
-              {selectedTab === "about-me" && (
-                <div className="grey-reactangle" style={{ zIndex: 1 }}></div>
-              )}
-            </div>
-
-            <div
-              className={`flex items-center gap-2 mb-5 relative cursor-pointer ${
-                selectedTab === "tech-skills"
-                  ? "cursor-default"
-                  : "cursor-pointer"
-              }`}
-              onClick={() => setSelectedTab("tech-skills")}
-            >
-              <div className="rounded border border-white star-container z-10">
-                <Image
-                  src="/images/tech-star.svg"
-                  width={20}
-                  height={20}
-                  alt="star shape"
-                />
-              </div>
-              <p className="text-xs font-medium z-10">Technical Skills</p>
-              {selectedTab === "tech-skills" && (
-                <div className="grey-reactangle" style={{ zIndex: 1 }}></div>
-              )}
-            </div>
-
-            <div
-              className={`flex items-center gap-2 mb-5 relative cursor-pointer ${
+              className={`flex items-center gap-2 mb-5 relative cursor-pointer pt-2 pb-2 pl-3 rounded-lg ${
                 selectedTab === "experience"
-                  ? "cursor-default"
+                  ? "cursor-default bg-slate-100"
                   : "cursor-pointer"
               }`}
               onClick={() => setSelectedTab("experience")}
@@ -70,55 +51,36 @@ const AboutPage = () => {
                 />
               </div>
               <p className="text-xs font-medium z-10">Experience</p>
-              {selectedTab === "experience" && (
-                <div className="grey-reactangle" style={{ zIndex: 1 }}></div>
-              )}
             </div>
           </div>
         </div>
         <div className="mt-12">
           <h2 className="text-xl font-semibold mb-5">Projects</h2>
+          <MenuItem
+            itemValue="portfolio"
+            selectedTab={selectedTab}
+            setSelectedTab={setSelectedTab}
+            image="p-icon.svg"
+            imageAlt="Profile icon"
+            menuText="Profile"
+            imageWidth={22}
+            imageHeight={22}
+          />
+          <MenuItem
+            itemValue="mern"
+            selectedTab={selectedTab}
+            setSelectedTab={setSelectedTab}
+            image="mern-icon.svg"
+            imageAlt="mern icon"
+            menuText="MERN + Firebase Auth"
+            imageWidth={22}
+            imageHeight={22}
+          />
           <div
-            className={`flex items-center gap-2 mb-5 relative cursor-pointer ${
-              selectedTab === "portfolio" ? "cursor-default" : "cursor-pointer"
-            }`}
-            onClick={() => setSelectedTab("portfolio")}
-          >
-            <div className="exp-container z-10">
-              <Image
-                src="/images/p-icon.svg"
-                width={22}
-                height={22}
-                alt="star shape"
-              />
-            </div>
-            <p className="text-xs font-medium z-10">Portfolio</p>
-            {selectedTab === "portfolio" && (
-              <div className="grey-reactangle" style={{ zIndex: 1 }}></div>
-            )}
-          </div>
-          <div
-            className={`flex items-center gap-2 mb-5 relative cursor-pointer ${
-              selectedTab === "mern" ? "cursor-default" : "cursor-pointer"
-            }`}
-            onClick={() => setSelectedTab("mern")}
-          >
-            <div className="rounded border border-white z-10">
-              <Image
-                src="/images/mern-icon.svg"
-                width={20}
-                height={20}
-                alt="star shape"
-              />
-            </div>
-            <p className="text-xs font-medium z-10">MERN + Firebase Auth</p>
-            {selectedTab === "mern" && (
-              <div className="grey-reactangle" style={{ zIndex: 1 }}></div>
-            )}
-          </div>
-          <div
-            className={`flex items-center gap-2 mb-5 relative cursor-pointer ${
-              selectedTab === "twitter" ? "cursor-default" : "cursor-pointer"
+            className={`flex items-center gap-2 mb-5 relative cursor-pointer pt-2 pb-2 pl-3 rounded-lg  ${
+              selectedTab === "twitter"
+                ? "cursor-default bg-slate-100"
+                : "cursor-pointer"
             }`}
             onClick={() => setSelectedTab("twitter")}
           >
@@ -131,9 +93,6 @@ const AboutPage = () => {
               />
             </div>
             <p className="text-xs font-medium z-10">Twitter Feed</p>
-            {selectedTab === "twitter" && (
-              <div className="grey-reactangle" style={{ zIndex: 1 }}></div>
-            )}
           </div>
         </div>
       </div>
