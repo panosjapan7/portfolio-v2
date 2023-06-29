393651a4172f0eeba18f7842dc71463c4bfb695c
panosjapan7 <panosjapan@gmail.com>
2023-06-29 12:15:48 +0200

    Creates About menu

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,39 +4,77 @@ import "./about.css";
 const AboutPage = () => {
   return (
     <div className="pl-11 pr-11 flex justify-center gap-10">
-      <div className="">
-        <h2 className="text-xl font-bold mb-5">About Me</h2>
-        <div className="flex items-center gap-2 mb-5">
-          <Image
-            className="rounded border border-white"
-            src="/images/panos-2-square.jpg"
-            width={23}
-            height={23}
-            alt="Panos"
-          />
-          <p className="text-xs font-medium">About Me</p>
-        </div>
-        <div className="flex items-center gap-2 mb-5">
-          <div className="rounded border border-white star-container">
+      <div className="left-column">
+        <div>
+          <h2 className="text-xl font-bold mb-5">About Me</h2>
+          <div className="flex items-center gap-2 mb-5">
             <Image
-              src="/images/tech-star.svg"
-              width={20}
-              height={20}
-              alt="star shape"
+              className="rounded border border-white"
+              src="/images/panos-2-square.jpg"
+              width={23}
+              height={23}
+              alt="Panos"
             />
+            <p className="text-xs font-medium">About Me</p>
           </div>
-          <p className="text-xs font-medium">Technical Skills</p>
+          <div className="flex items-center gap-2 mb-5">
+            <div className="rounded border border-white star-container">
+              <Image
+                src="/images/tech-star.svg"
+                width={20}
+                height={20}
+                alt="star shape"
+              />
+            </div>
+            <p className="text-xs font-medium">Technical Skills</p>
+          </div>
+          <div className="flex items-center gap-2 mb-5">
+            <div className="exp-container">
+              <Image
+                src="/images/exp-star.png"
+                width={12}
+                height={12}
+                alt="star shape"
+              />
+            </div>
+            <p className="text-xs font-medium">Experience</p>
+          </div>
         </div>
-        <div className="flex items-center gap-2 mb-5">
-          <div className="exp-container">
-            <Image
-              src="/images/exp-star.png"
-              width={12}
-              height={12}
-              alt="star shape"
-            />
+        <div className="mt-11">
+          <h2 className="text-xl font-bold mb-5">Projects</h2>
+          <div className="flex items-center gap-2 mb-5">
+            <div className="exp-container">
+              <Image
+                src="/images/p-icon.svg"
+                width={22}
+                height={22}
+                alt="star shape"
+              />
+            </div>
+            <p className="text-xs font-medium">Portfolio</p>
           </div>
-          <p className="text-xs font-medium">Experience</p>
+          <div className="flex items-center gap-2 mb-5">
+            <div className="rounded border border-white star-container">
+              <Image
+                src="/images/mern-icon.svg"
+                width={20}
+                height={20}
+                alt="star shape"
+              />
+            </div>
+            <p className="text-xs font-medium">MERN + Firebase Auth</p>
+          </div>
+          <div className="flex items-center gap-2 mb-5">
+            <div className="exp-container">
+              <Image
+                src="/images/twitter-icon.png"
+                width={12}
+                height={12}
+                alt="star shape"
+              />
+            </div>
+            <p className="text-xs font-medium">Twitter Feed</p>
+          </div>
         </div>
       </div>
       <div className="border border-black row-span-3 col-span-4">
