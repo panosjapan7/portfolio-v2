fe066f8765aa92d02879e91092f2ea9b11f715e6
panosjapan7 <panosjapan@gmail.com>
2023-07-01 11:04:52 +0200

    Creates tech skills of second row

--- app/about/components/TechnicalSkills.tsx
+++ app/about/components/TechnicalSkills.tsx
@@ -148,6 +148,90 @@ const TechnicalSkills = () => {
           </div>
         </div>
       </div>
+      <div
+        style={{
+          border: "1px solid #dddddd",
+          marginTop: 16,
+          marginBottom: 16,
+          //   maxWidth: 512,
+          width: "100%",
+          boxSizing: "border-box",
+        }}
+      ></div>
+      <div className="grid grid-cols-3 gap-10">
+        <div>
+          <h3 className="text-slate-800 font-medium">Backend</h3>
+          <TechnicalSkill
+            source="/images/javascript-icon.svg"
+            alt="javascript icon"
+            width={14}
+            height={14}
+            skill="JavaSciprt"
+          />
+          <TechnicalSkill
+            source="/images/node-icon.svg"
+            alt="node.js icon"
+            width={14}
+            height={8}
+            skill="Node.js"
+          />
+          <TechnicalSkill
+            source="/images/typescript-icon.svg"
+            alt="typescript icon"
+            width={14}
+            height={14}
+            skill="TypeScript"
+          />
+        </div>
+        <div>
+          <h3 className="text-slate-800 font-medium">DBs / APIs</h3>
+          <TechnicalSkill
+            source="/images/firebase-icon.svg"
+            alt="firebase icon"
+            width={11}
+            height={14}
+            skill="Firebase"
+          />
+          <TechnicalSkill
+            source="/images/mongodb-icon.svg"
+            alt="mongodb icon"
+            width={9}
+            height={2}
+            skill="MongoDB"
+          />
+          <TechnicalSkill
+            source="/images/postman-icon.svg"
+            alt="postman icon"
+            width={14}
+            height={14}
+            skill="Postman"
+          />
+          <TechnicalSkill
+            source="/images/rest-api-icon.svg"
+            alt="rest api icon"
+            width={14}
+            height={11}
+            skill="REST API"
+          />
+        </div>
+        <div>
+          <h3 className="text-slate-800 font-medium">Version Control</h3>
+          <TechnicalSkill
+            source="/images/git-icon.svg"
+            alt="git icon"
+            width={14}
+            height={14}
+            skill="Git"
+          />
+          <TechnicalSkill
+            source="/images/github-icon.svg"
+            alt="github icon"
+            width={14}
+            height={14}
+            skill="GitHub"
+          />
+        </div>
+      </div>
     </div>
   );
 };
