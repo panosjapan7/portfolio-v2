31d50a3d9a6e4536e560544299e7ceb0b765f9dd
panosjapan7 <panosjapan@gmail.com>
2023-07-01 10:35:45 +0200

    Creates TechnicalSkill component

---
+++ app/about/components/TechnicalSkill.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+interface TechSkill {
+  source: string;
+  alt: string;
+  width: number;
+  height: number;
+  skill: string;
+}
+
+const TechnicalSkill: React.FC<TechSkill> = ({
+  source,
+  alt,
+  width,
+  height,
+  skill,
+}) => {
+  return (
+    <div className="flex gap-1 ml-2 mb-2">
+      <Image src={source} alt={alt} width={width} height={height} />
+      <p className="text-sm font-light">{skill}</p>
+    </div>
+  );
+};
+
+export default TechnicalSkill;
