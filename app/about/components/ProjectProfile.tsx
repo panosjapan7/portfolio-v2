829c62852baa15d682b4e70eb7b7ca548caaba94
panosjapan7 <panosjapan@gmail.com>
2023-07-02 09:21:12 +0200

    Adds frontend, backend, github, link props

--- app/about/components/ProjectProfile.tsx
+++ app/about/components/ProjectProfile.tsx
@@ -15,6 +15,14 @@ const ProjectProfile = () => {
         "To add an 'admin' role to a user, you need to manually edit the user entry on MongoDB.",
         'The screen "Admin Page (protected)" can be viewed only if the logged-in user has an "admin" role; if not, it redirects back to Home page.',
       ]}
+      frontend={
+        "Figma, Framer Motion, Next JS, Tailwind, TypeScript, Weather API"
+      }
+      backend={
+        "Figma, Framer Motion, Next JS, Tailwind, TypeScript, Weather API"
+      }
+      github="https:/www.google.com"
+      link="https:/www.google.com"
     />
   );
 };
