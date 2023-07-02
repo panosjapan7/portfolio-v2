8040156f3cf8af2366b84fca17c49a88e7443cf5
panosjapan7 <panosjapan@gmail.com>
2023-07-02 09:04:27 +0200

    Renders ProjectItem for Profile project dynamically

---
+++ app/about/components/ProjectProfile.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ProjectItem from "./ProjectItem";
+
+const ProjectProfile = () => {
+  return (
+    <ProjectItem
+      projectTitle="Portfolio Website"
+      description={[
+        "I’ve always approached minimal design in a very clean and clinical way so I wanted to explore a minimal direction that has warmth to it.",
+      ]}
+      features={[
+        "The user receives an email to verify their email address.",
+        "The user can reset their password.",
+        "The app uses a backend server to create a user entry on MongoDB that includes among other properties a default 'user' role.",
+        "To add an 'admin' role to a user, you need to manually edit the user entry on MongoDB.",
+        'The screen "Admin Page (protected)" can be viewed only if the logged-in user has an "admin" role; if not, it redirects back to Home page.',
+      ]}
+    />
+  );
+};
+
+export default ProjectProfile;
