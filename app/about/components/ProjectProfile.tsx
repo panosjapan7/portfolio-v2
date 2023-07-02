23b4c342bbfc17222015ae723d78aa320bb405cc
panosjapan7 <panosjapan@gmail.com>
2023-07-02 10:41:41 +0200

    Adds bg image

--- app/about/components/ProjectProfile.tsx
+++ app/about/components/ProjectProfile.tsx
@@ -3,24 +3,44 @@ import ProjectItem from "./ProjectItem";
 
 const ProjectProfile = () => {
   return (
-    <ProjectItem
-      projectTitle="Portfolio Website"
-      description={[
-        "I’ve always approached minimal design in a very clean and clinical way so I wanted to explore a minimal direction that has warmth to it.",
-      ]}
-      features={[
-        "The user receives an email to verify their email address.",
-        "The user can reset their password.",
-        "The app uses a backend server to create a user entry on MongoDB that includes among other properties a default 'user' role.",
-        "To add an 'admin' role to a user, you need to manually edit the user entry on MongoDB.",
-        'The screen "Admin Page (protected)" can be viewed only if the logged-in user has an "admin" role; if not, it redirects back to Home page.',
-      ]}
-      frontend={
-        "Figma, Framer Motion, Next JS, Tailwind, TypeScript, Weather API"
-      }
-      github="https:/www.google.com"
-      link="https:/www.google.com"
-    />
+    <div className="relative">
+      <ProjectItem
+        projectTitle="Portfolio Website"
+        description={[
+          "I’ve always approached minimal design in a very clean and clinical way so I wanted to explore a minimal direction that has warmth to it.",
+        ]}
+        features={[
+          "The user receives an email to verify their email address.",
+          "The user can reset their password.",
+          "The app uses a backend server to create a user entry on MongoDB that includes among other properties a default 'user' role.",
+          "To add an 'admin' role to a user, you need to manually edit the user entry on MongoDB.",
+          'The screen "Admin Page (protected)" can be viewed only if the logged-in user has an "admin" role; if not, it redirects back to Home page.',
+        ]}
+        frontend={
+          "Figma, Framer Motion, Next JS, Tailwind, TypeScript, Weather API"
+        }
+        github="https:/www.google.com"
+        link="https:/www.google.com"
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: 0,
+          right: "15%",
+          margin: "auto",
+          transform: "translateY(-50%)",
+          textAlign: "center",
+          zIndex: 10,
+
+          width: "301px",
+          height: "301px",
+          background: "#ffc43f",
+          opacity: 0.45,
+          filter: "blur(50px)",
+        }}
+      ></div>
+    </div>
   );
 };
 
