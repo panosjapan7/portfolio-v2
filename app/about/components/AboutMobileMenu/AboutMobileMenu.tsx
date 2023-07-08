4a4e8a808d5414c00d35a8368a9e707395c064ec
panosjapan7 <panosjapan@gmail.com>
2023-07-08 11:26:25 +0700

    Places AboutMobileMenu.tsx correctly

--- app/about/components/AboutMobileMenu/AboutMobileMenu.tsx
+++ app/about/components/AboutMobileMenu/AboutMobileMenu.tsx
@@ -4,51 +4,114 @@ import { IoDocumentsOutline } from "react-icons/io5";
 import "./aboutMobileMenu.css";
 
 const AboutMobileMenu = () => {
-  const [selected, setSelected] = useState("about-me");
-
-  const handleOptionClick = (option: string) => {
-    setSelected(option);
-  };
+  const [selectedOption, setSelectedOption] = useState("about-me");
+  const [selectedSuboption, setSelectedSuboption] =
+    useState("portfolio-website");
 
   return (
-    <div className="flex gap-9 flex-wrap p-2 relative">
-      <div
-        className={`option-container ${
-          selected === "about-me" ? "active" : ""
-        }`}
-        onClick={() => setSelected("about-me")}
-      >
-        <FaUser
-          style={{
-            height: "16px",
-            width: "16px",
-            marginTop: -4,
-            color: "#9B9B9B",
-          }}
-        />
-        <h2 className="text-base" style={{ color: "#9B9B9B" }}>
-          About Me
-        </h2>
+    <div className="ml-6">
+      <div className="flex">
+        <div className="flex gap-3 flex-wrap">
+          <div
+            className={`option-container ${
+              selectedOption === "about-me" ? "active" : ""
+            }`}
+            onClick={() => setSelectedOption("about-me")}
+          >
+            <FaUser
+              style={{
+                height: "16px",
+                width: "16px",
+                marginTop: -4,
+                color: "#9B9B9B",
+              }}
+            />
+            <h2 className="text-base" style={{ color: "#9B9B9B" }}>
+              About Me
+            </h2>
+          </div>
+          <div
+            className={`option-container ${
+              selectedOption === "projects" ? "active" : ""
+            }`}
+            onClick={() => setSelectedOption("projects")}
+          >
+            <IoDocumentsOutline
+              style={{
+                height: "18px",
+                width: "18px",
+                marginTop: 0,
+                color: "#9B9B9B",
+              }}
+            />
+            <h2 className="text-base" style={{ color: "#9B9B9B" }}>
+              Projects
+            </h2>
+          </div>
+        </div>
       </div>
 
-      <div
-        className={`option-container ${
-          selected === "projects" ? "active" : ""
-        }`}
-        onClick={() => setSelected("projects")}
-      >
-        <IoDocumentsOutline
-          style={{
-            height: "18px",
-            width: "18px",
-            marginTop: 0,
-            color: "#9B9B9B",
-          }}
-        />
-        <h2 className="text-base" style={{ color: "#9B9B9B" }}>
-          Projects
-        </h2>
-      </div>
+      {selectedOption === "about-me" && (
+        <div className="sub-options-container">
+          <div
+            className={`sub-option-container ${
+              selectedSuboption === "portfolio-website"
+                ? "sub-option-active"
+                : ""
+            }`}
+            onClick={() => setSelectedSuboption("portfolio-website")}
+          >
+            <h3 className="text-sm">About Me</h3>
+          </div>
+          <div
+            className={`sub-option-container ${
+              selectedSuboption === "mern" ? "sub-option-active" : ""
+            }`}
+            onClick={() => setSelectedSuboption("mern")}
+          >
+            <h3 className="text-sm">Technical Skills</h3>
+          </div>
+          <div
+            className={`sub-option-container ${
+              selectedSuboption === "twitter-feed" ? "sub-option-active" : ""
+            }`}
+            onClick={() => setSelectedSuboption("twitter-feed")}
+          >
+            <h3 className="text-sm">Experience</h3>
+          </div>
+        </div>
+      )}
+
+      {selectedOption === "projects" && (
+        <div className="sub-options-container">
+          <div
+            className={`sub-option-container ${
+              selectedSuboption === "portfolio-website"
+                ? "sub-option-active"
+                : ""
+            }`}
+            onClick={() => setSelectedSuboption("portfolio-website")}
+          >
+            <h3 className="text-sm">Portfolio Website</h3>
+          </div>
+          <div
+            className={`sub-option-container ${
+              selectedSuboption === "mern" ? "sub-option-active" : ""
+            }`}
+            onClick={() => setSelectedSuboption("mern")}
+          >
+            <h3 className="text-sm">MERN + Firebase Auth</h3>
+          </div>
+          <div
+            className={`sub-option-container ${
+              selectedSuboption === "twitter-feed" ? "sub-option-active" : ""
+            }`}
+            onClick={() => setSelectedSuboption("twitter-feed")}
+          >
+            <h3 className="text-sm">Twitter Feed</h3>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,108 +15,110 @@ import AboutMobileMenu from "./components/AboutMobileMenu/AboutMobileMenu";
 const AboutPage = () => {
   const [selectedTab, setSelectedTab] = useState("about-me");
   return (
-    <div className="flex justify-center">
-      <div className="mt-0 pb-5 pl-11 pr-11 justify-center self-center gap-10 lg:mt-0 md:mt-10 sm2:mt-10 sm:mt-10 grid grid-cols-4 md3:hidden">
-        <div className="left-column col-span-1">
-          <div>
-            <h2 className="text-xl font-semibold mb-2">About Me</h2>
-
+    <>
+      <div className="flex justify-center">
+        <div className="mt-0 pb-5 pl-11 pr-11 justify-center self-center gap-10 lg:mt-0 md:mt-10 sm2:mt-10 sm:mt-10 grid grid-cols-4 md3:hidden">
+          <div className="left-column col-span-1">
             <div>
+              <h2 className="text-xl font-semibold mb-2">About Me</h2>
+
+              <div>
+                <MenuItem
+                  itemValue="about-me"
+                  selectedTab={selectedTab}
+                  setSelectedTab={setSelectedTab}
+                  image="panos-2-square.jpg"
+                  imageAlt="Panos"
+                  menuText="About Me"
+                  imageWidth={22}
+                  imageHeight={22}
+                />
+                <MenuItem
+                  itemValue="tech-skills"
+                  selectedTab={selectedTab}
+                  setSelectedTab={setSelectedTab}
+                  image="tech-star.svg"
+                  imageAlt="Tech icon"
+                  menuText="Technical Skills"
+                  imageWidth={22}
+                  imageHeight={22}
+                />
+                <div
+                  className={`flex items-center gap-2 mb-5 relative cursor-pointer pt-2 pb-2 pl-3 rounded-lg ${
+                    selectedTab === "experience"
+                      ? "cursor-default bg-slate-100"
+                      : "cursor-pointer"
+                  }`}
+                  onClick={() => setSelectedTab("experience")}
+                >
+                  <div className="exp-container z-10">
+                    <Image
+                      src="/images/exp-star.png"
+                      width={12}
+                      height={12}
+                      alt="star shape"
+                    />
+                  </div>
+                  <p className="text-xs font-medium z-10">Experience</p>
+                </div>
+              </div>
+            </div>
+            <div className="mt-6">
+              <h2 className="text-xl font-semibold mb-2">Projects</h2>
               <MenuItem
-                itemValue="about-me"
+                itemValue="portfolio"
                 selectedTab={selectedTab}
                 setSelectedTab={setSelectedTab}
-                image="panos-2-square.jpg"
-                imageAlt="Panos"
-                menuText="About Me"
+                image="p-icon.svg"
+                imageAlt="Profile icon"
+                menuText="Portfolio Website"
                 imageWidth={22}
                 imageHeight={22}
               />
               <MenuItem
-                itemValue="tech-skills"
+                itemValue="mern"
                 selectedTab={selectedTab}
                 setSelectedTab={setSelectedTab}
-                image="tech-star.svg"
-                imageAlt="Tech icon"
-                menuText="Technical Skills"
+                image="mern-icon.svg"
+                imageAlt="mern icon"
+                menuText="MERN + Firebase Auth"
                 imageWidth={22}
                 imageHeight={22}
               />
               <div
-                className={`flex items-center gap-2 mb-5 relative cursor-pointer pt-2 pb-2 pl-3 rounded-lg ${
-                  selectedTab === "experience"
+                className={`flex items-center gap-2 mb-5 relative cursor-pointer pt-2 pb-2 pl-3 rounded-lg  ${
+                  selectedTab === "twitter"
                     ? "cursor-default bg-slate-100"
                     : "cursor-pointer"
                 }`}
-                onClick={() => setSelectedTab("experience")}
+                onClick={() => setSelectedTab("twitter")}
               >
                 <div className="exp-container z-10">
                   <Image
-                    src="/images/exp-star.png"
+                    src="/images/twitter-icon.png"
                     width={12}
                     height={12}
                     alt="star shape"
                   />
                 </div>
-                <p className="text-xs font-medium z-10">Experience</p>
+                <p className="text-xs font-medium z-10">Twitter Feed</p>
               </div>
             </div>
           </div>
-          <div className="mt-6">
-            <h2 className="text-xl font-semibold mb-2">Projects</h2>
-            <MenuItem
-              itemValue="portfolio"
-              selectedTab={selectedTab}
-              setSelectedTab={setSelectedTab}
-              image="p-icon.svg"
-              imageAlt="Profile icon"
-              menuText="Portfolio Website"
-              imageWidth={22}
-              imageHeight={22}
-            />
-            <MenuItem
-              itemValue="mern"
-              selectedTab={selectedTab}
-              setSelectedTab={setSelectedTab}
-              image="mern-icon.svg"
-              imageAlt="mern icon"
-              menuText="MERN + Firebase Auth"
-              imageWidth={22}
-              imageHeight={22}
-            />
-            <div
-              className={`flex items-center gap-2 mb-5 relative cursor-pointer pt-2 pb-2 pl-3 rounded-lg  ${
-                selectedTab === "twitter"
-                  ? "cursor-default bg-slate-100"
-                  : "cursor-pointer"
-              }`}
-              onClick={() => setSelectedTab("twitter")}
-            >
-              <div className="exp-container z-10">
-                <Image
-                  src="/images/twitter-icon.png"
-                  width={12}
-                  height={12}
-                  alt="star shape"
-                />
-              </div>
-              <p className="text-xs font-medium z-10">Twitter Feed</p>
-            </div>
+          <div className="row-span-3 col-span-3">
+            {selectedTab === "about-me" && <AboutMe />}
+            {selectedTab === "tech-skills" && <TechnicalSkills />}
+            {selectedTab === "experience" && <Experience />}
+            {selectedTab === "portfolio" && <ProjectPortfolio />}
+            {selectedTab === "mern" && <ProjectMern />}
+            {selectedTab === "twitter" && <ProjectTwitter />}
           </div>
-        </div>
-        <div className="row-span-3 col-span-3">
-          {selectedTab === "about-me" && <AboutMe />}
-          {selectedTab === "tech-skills" && <TechnicalSkills />}
-          {selectedTab === "experience" && <Experience />}
-          {selectedTab === "portfolio" && <ProjectPortfolio />}
-          {selectedTab === "mern" && <ProjectMern />}
-          {selectedTab === "twitter" && <ProjectTwitter />}
         </div>
       </div>
-      <div className="md3:visible md4:hidden pt-5" style={{ maxWidth: 475 }}>
+      <div className="md3:visible md4:hidden pt-6">
         <AboutMobileMenu />
       </div>
-    </div>
+    </>
   );
 };
 
