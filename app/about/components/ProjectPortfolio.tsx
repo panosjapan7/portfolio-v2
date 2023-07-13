69c1e9207fece37f0f87995455aadd73e8b3e25d
panosjapan7 <panosjapan@gmail.com>
2023-07-13 14:38:48 +0700

    Shows portfolio on mobile view

--- app/about/components/ProjectPortfolio.tsx
+++ app/about/components/ProjectPortfolio.tsx
@@ -1,44 +1,48 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+import "./projectPortfolio.css";
 
 const ProjectPortfolio = () => {
   return (
-    <div className="relative">
-      <ProjectItem
-        projectTitle="Portfolio Website"
-        description={[
-          "I’ve always approached minimal design in a very clean and clinical way so I wanted to explore a minimal direction that has warmth to it.",
-          "Another thing I wanted to do is get myself familiar with Next.js and Tailwind so I used them to build this project.",
-          "I'm also really passionate about scalable and readable code so I used child components that receive props to dynamically generate components by passing just the content data to them.",
-        ]}
-        features={[
-          "The home page fetches data from Weather API to show weather conditions in Stockholm.",
-          "The home page displays the time and date in Stockholm.",
-          "The menu in desktop resolutions was built with Framer Motion to simulate the menu of Apple's Mac OS.",
-        ]}
-        frontend={
-          "Figma, Framer Motion, Next JS, Tailwind, TypeScript, Weather API"
-        }
-        github="https://github.com/panosjapan7/portfolio-v2"
-      />
-      <div
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: 0,
-          right: "15%",
-          margin: "auto",
-          transform: "translateY(-50%)",
-          textAlign: "center",
-          zIndex: 10,
+    <div className="relative md3:p-5">
+      <div className="content-wrapper">
+        <ProjectItem
+          projectTitle="Portfolio Website"
+          description={[
+            "I’ve always approached minimal design in a very clean and clinical way so I wanted to explore a minimal direction that has warmth to it.",
+            "Another thing I wanted to do is get myself familiar with Next.js and Tailwind so I used them to build this project.",
+            "I'm also really passionate about scalable and readable code so I used child components that receive props to dynamically generate components by passing just the content data to them.",
+          ]}
+          features={[
+            "The home page fetches data from Weather API to show weather conditions in Stockholm.",
+            "The home page displays the time and date in Stockholm.",
+            "The menu in desktop resolutions was built with Framer Motion to simulate the menu of Apple's Mac OS.",
+          ]}
+          frontend={
+            "Figma, Framer Motion, Next JS, Tailwind, TypeScript, Weather API"
+          }
+          github="https://github.com/panosjapan7/portfolio-v2"
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: 0,
+            right: "15%",
+            margin: "auto",
+            transform: "translateY(-50%)",
+            textAlign: "center",
+            zIndex: 10,
 
-          width: "301px",
-          height: "301px",
-          background: "#ffc43f",
-          opacity: 0.4,
-          filter: "blur(50px)",
-        }}
-      ></div>
+            width: "301px",
+            height: "301px",
+            background: "#ffc43f",
+            opacity: 0.4,
+            filter: "blur(50px)",
+          }}
+        ></div>
+      </div>
+      <div className="fade-block"></div>
     </div>
   );
 };
