99822546c06cf1bc3af820ba8a8eda5db5fccf93
panosjapan7 <panosjapan@gmail.com>
2023-07-17 16:11:15 +0200

    Gets isDay and passes it as a prop to Weather component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 "use client";
+import moment from "moment";
+import "moment-timezone";
 import Weather from "./components/Weather/Weather";
 
 export default function Home() {
+  moment.locale("en");
+  moment.tz.setDefault("Europe/Stockholm");
+  const currentHour = moment().format("HH");
+  let isDay = false;
+  // if (+currentHour > 6 && +currentHour < 20) {
+  //   isDay = true;
+  // }
+
   return (
     <main>
-      <Weather />
+      <Weather isDay={isDay} />
     </main>
   );
 }
