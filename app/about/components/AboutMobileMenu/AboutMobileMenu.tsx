d4722b6b86e2e501d5826fb1b73587bdf94ce552
panosjapan7 <panosjapan@gmail.com>
2023-07-18 08:54:14 +0200

    Changes color of suboptions menu, bgr based on isDay

--- app/about/components/AboutMobileMenu/AboutMobileMenu.tsx
+++ app/about/components/AboutMobileMenu/AboutMobileMenu.tsx
@@ -33,6 +33,31 @@ const AboutMobileMenu: React.FC<MobileMenu> = ({
     selectedTabMobile === "projects" ? "active" : ""
   }`;
 
+  const suboptionContainerClassname = `sub-option-container ${
+    selectedAboutSuboptionMobile === "about-me" ? "sub-option-active" : ""
+  }`;
+
+  const suboptions = [
+    "about-me",
+    "tech-skills",
+    "experience",
+    "portfolio-website",
+    "mern",
+    "twitter",
+  ];
+
+  const aboutSuboptionsContainerClassName = (suboption: string) => {
+    return `sub-option-container ${
+      selectedAboutSuboptionMobile === suboption ? "sub-option-active" : ""
+    }`;
+  };
+
+  const projectsSuboptionsContainerClassName = (suboption: string) => {
+    return `sub-option-container ${
+      selectedProjectsSuboptionMobile === suboption ? "sub-option-active" : ""
+    }`;
+  };
+
   return (
     <div className="ml-6 mr-3">
       <div className="flex">
@@ -95,34 +120,64 @@ const AboutMobileMenu: React.FC<MobileMenu> = ({
       {selectedTabMobile === "about-me" && (
         <div className="sub-options-container">
           <div
-            className={`sub-option-container ${
-              selectedAboutSuboptionMobile === "about-me"
-                ? "sub-option-active"
-                : ""
-            }`}
+            className={aboutSuboptionsContainerClassName("about-me")}
+            style={{
+              background: aboutSuboptionsContainerClassName(
+                "about-me"
+              ).includes("sub-option-active")
+                ? isDay
+                  ? "rgba(255, 255, 255, 0.4)"
+                  : "rgba(59, 59, 59, 0.1)"
+                : "unset",
+            }}
             onClick={() => setSelectedAboutSuboptionMobile("about-me")}
           >
-            <h3 className="text-sm">About Me</h3>
+            <h3
+              className="text-sm"
+              style={{ color: isDay ? "#9b9b9b" : "white" }}
+            >
+              About Me
+            </h3>
           </div>
           <div
-            className={`sub-option-container ${
-              selectedAboutSuboptionMobile === "tech-skills"
-                ? "sub-option-active"
-                : ""
-            }`}
+            className={aboutSuboptionsContainerClassName("tech-skills")}
+            style={{
+              background: aboutSuboptionsContainerClassName(
+                "tech-skills"
+              ).includes("sub-option-active")
+                ? isDay
+                  ? "rgba(255, 255, 255, 0.4)"
+                  : "rgba(59, 59, 59, 0.1)"
+                : "unset",
+            }}
             onClick={() => setSelectedAboutSuboptionMobile("tech-skills")}
           >
-            <h3 className="text-sm">Technical Skills</h3>
+            <h3
+              className="text-sm"
+              style={{ color: isDay ? "#9b9b9b" : "white" }}
+            >
+              Technical Skills
+            </h3>
           </div>
           <div
-            className={`sub-option-container ${
-              selectedAboutSuboptionMobile === "experience"
-                ? "sub-option-active"
-                : ""
-            }`}
+            className={aboutSuboptionsContainerClassName("experience")}
+            style={{
+              background: aboutSuboptionsContainerClassName(
+                "experience"
+              ).includes("sub-option-active")
+                ? isDay
+                  ? "rgba(255, 255, 255, 0.4)"
+                  : "rgba(59, 59, 59, 0.1)"
+                : "unset",
+            }}
             onClick={() => setSelectedAboutSuboptionMobile("experience")}
           >
-            <h3 className="text-sm">Experience</h3>
+            <h3
+              className="text-sm"
+              style={{ color: isDay ? "#9b9b9b" : "white" }}
+            >
+              Experience
+            </h3>
           </div>
         </div>
       )}
@@ -130,36 +185,68 @@ const AboutMobileMenu: React.FC<MobileMenu> = ({
       {selectedTabMobile === "projects" && (
         <div className="sub-options-container">
           <div
-            className={`sub-option-container ${
-              selectedProjectsSuboptionMobile === "portfolio-website"
-                ? "sub-option-active"
-                : ""
-            }`}
+            className={projectsSuboptionsContainerClassName(
+              "portfolio-website"
+            )}
+            style={{
+              background: projectsSuboptionsContainerClassName(
+                "portfolio-website"
+              ).includes("sub-option-active")
+                ? isDay
+                  ? "rgba(255, 255, 255, 0.4)"
+                  : "rgba(59, 59, 59, 0.1)"
+                : "unset",
+            }}
             onClick={() =>
               setSelectedProjectsSuboptionMobile("portfolio-website")
             }
           >
-            <h3 className="text-sm">Portfolio Website</h3>
+            <h3
+              className="text-sm"
+              style={{ color: isDay ? "#9b9b9b" : "white" }}
+            >
+              Portfolio Website
+            </h3>
           </div>
           <div
-            className={`sub-option-container ${
-              selectedProjectsSuboptionMobile === "mern"
-                ? "sub-option-active"
-                : ""
-            }`}
+            className={projectsSuboptionsContainerClassName("mern")}
+            style={{
+              background: projectsSuboptionsContainerClassName("mern").includes(
+                "sub-option-active"
+              )
+                ? isDay
+                  ? "rgba(255, 255, 255, 0.4)"
+                  : "rgba(59, 59, 59, 0.1)"
+                : "unset",
+            }}
             onClick={() => setSelectedProjectsSuboptionMobile("mern")}
           >
-            <h3 className="text-sm">MERN + Firebase Auth</h3>
+            <h3
+              className="text-sm"
+              style={{ color: isDay ? "#9b9b9b" : "white" }}
+            >
+              MERN + Firebase Auth
+            </h3>
           </div>
           <div
-            className={`sub-option-container ${
-              selectedProjectsSuboptionMobile === "twitter"
-                ? "sub-option-active"
-                : ""
-            }`}
+            className={projectsSuboptionsContainerClassName("twitter")}
+            style={{
+              background: projectsSuboptionsContainerClassName(
+                "twitter"
+              ).includes("sub-option-active")
+                ? isDay
+                  ? "rgba(255, 255, 255, 0.4)"
+                  : "rgba(59, 59, 59, 0.1)"
+                : "unset",
+            }}
             onClick={() => setSelectedProjectsSuboptionMobile("twitter")}
           >
-            <h3 className="text-sm">Twitter Feed</h3>
+            <h3
+              className="text-sm"
+              style={{ color: isDay ? "#9b9b9b" : "white" }}
+            >
+              Twitter Feed
+            </h3>
           </div>
         </div>
       )}
