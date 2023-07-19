f5a0dc4dfca0b3f112a4bb411f908408154d3b85
panosjapan7 <panosjapan@gmail.com>
2023-07-19 13:20:37 +0200

    Removes unused code

--- app/components/Weather/conditions/Drizzle.tsx
+++ app/components/Weather/conditions/Drizzle.tsx
@@ -1,11 +1,9 @@
 import "./conditions.css";
-import { RiDrizzleFill } from "react-icons/ri";
 import { CiCloudDrizzle } from "react-icons/ci";
 
 const Drizzle: React.FC = () => {
   return (
     <div className="drizzle-container">
-      {/* <RiDrizzleFill className="drizzle" /> */}
       <CiCloudDrizzle className="drizzle" />
     </div>
   );
