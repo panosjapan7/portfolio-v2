f3c774b9b757af0d2af8055f6c06f7ca139aa4e9
panosjapan7 <panosjapan@gmail.com>
2023-07-20 08:31:19 +0200

    Creates thunderstorm with new image

--- app/components/Weather/conditions/Thunderstorm.tsx
+++ app/components/Weather/conditions/Thunderstorm.tsx
@@ -1,11 +1,16 @@
+import Image from "next/image";
 import { AiFillThunderbolt, AiFillCloud } from "react-icons/ai";
 import "./conditions.css";
 
 const Thunderstorm: React.FC = () => {
   return (
-    <div className="thunderstorm-container">
-      <AiFillCloud className="thunderstorm-cloud" />
-      <AiFillThunderbolt className="thunderstorm-thunder" />
+    <div>
+      <Image
+        src={"/images/new/thunderstorm-new.png"}
+        width={120}
+        height={120}
+        alt="thunderstorm icon"
+      />
     </div>
   );
 };
